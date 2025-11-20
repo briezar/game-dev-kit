2e61be474d56a2317f76ed4db0349217623ac454
vietlq <vietlq@athena.studio>
2025-11-20 21:00:10 +0700

    Add GridExtensions. Change UIGraphicsRaycastEmpty -> EmptyRaycastableGraphic.

---
+++ Runtime/UI/EmptyRaycastableGraphic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.UI;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace GameDevKit.UI
+{
+    [RequireComponent(typeof(CanvasRenderer))]
+    public class EmptyRaycastableGraphic : Graphic
+    {
+        protected override void UpdateGeometry() { }
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(EmptyRaycastableGraphic))]
+    internal class EmptyRaycastableGraphicDrawer : GraphicEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(m_Script);
+            // AppearanceControlsGUI();
+            RaycastControlsGUI();
+            // MaskableControlsGUI();
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+#endif
+
+}
