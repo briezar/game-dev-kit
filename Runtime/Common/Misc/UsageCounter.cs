52d4211542d3ada3447c1953554a513195c330d9
vietlq <vietlq1998@gmail.com>
2026-06-26 12:12:11 +0700

    Add Coordinate2D

--- Runtime/Common/Misc/UsageCounter.cs
+++ Runtime/Common/Misc/UsageCounter.cs
@@ -2,12 +2,27 @@ using UnityEngine;
 
 namespace GameDevKit
 {
+    /// <summary>
+    /// Tracks whether a resource is in use using a simple reference-like counter.
+    /// </summary>
     public class UsageCounter
     {
+        /// <summary>
+        /// Gets the current number of active uses.
+        /// </summary>
         public int UseCount { get; private set; } = 0;
 
+        /// <summary>
+        /// Gets a value indicating whether the counter currently represents an active use.
+        /// </summary>
         public bool IsUsing => UseCount > 0;
 
+        /// <summary>
+        /// Adjusts the counter and reports whether the overall using state changed.
+        /// </summary>
+        /// <param name="use">If true, increments the counter; otherwise decrements it.</param>
+        /// <param name="usingStateChanged">True when the <see cref="IsUsing"/> state changes as a result of this call.</param>
+        /// <returns>True if the counter is currently in use after the change; otherwise false.</returns>
         public bool Use(bool use, out bool usingStateChanged)
         {
             var prev = IsUsing;
@@ -15,6 +30,12 @@ namespace GameDevKit
             usingStateChanged = prev != current;
             return current;
         }
+
+        /// <summary>
+        /// Adjusts the counter by one step.
+        /// </summary>
+        /// <param name="use">If true, increments the counter; otherwise decrements it.</param>
+        /// <returns>True if the counter is currently in use after the change; otherwise false.</returns>
         public bool Use(bool use)
         {
             UseCount += use ? 1 : -1;
@@ -22,9 +43,20 @@ namespace GameDevKit
             return IsUsing;
         }
 
+        /// <summary>
+        /// Increments the active use count by one.
+        /// </summary>
         public void Increment() => Use(true);
+
+        /// <summary>
+        /// Decrements the active use count by one.
+        /// </summary>
+        /// <returns>True if the counter is currently in use after the decrement; otherwise false.</returns>
         public bool Decrement() => Use(false);
 
+        /// <summary>
+        /// Resets the active use count to zero.
+        /// </summary>
         public void Reset() => UseCount = 0;
     }
 }
