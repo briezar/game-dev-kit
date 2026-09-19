2057ffa2e1f22d0bf91a67a7b6e09bcefed6ce43
briezar <vietlq1998@gmail.com>
2026-09-19 18:46:15 +0700

    Add comparison to IntAmount

--- Runtime/Misc/Numbers/Amounts/IntAmount.cs
+++ Runtime/Misc/Numbers/Amounts/IntAmount.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.Pool;
 
 namespace GameDevKit
 {
@@ -14,46 +16,69 @@ namespace GameDevKit
 
         public IntAmount(T item, int amount = 0) => (this.item, this.amount) = (item, amount);
 
-        private static void AssertEqualResource(IntAmount<T> left, IntAmount<T> right)
+        private static void AssertEqualItem(IntAmount<T> left, IntAmount<T> right)
         {
             Assert.AreEqual(left.item, right.item, $"Cannot operate on amount values with different items: '{left.item}' != '{right.item}'");
         }
 
         public static IntAmount<T> operator +(IntAmount<T> left, IntAmount<T> right)
         {
-            AssertEqualResource(left, right);
+            AssertEqualItem(left, right);
             return new(left.item, left.amount + right.amount);
         }
 
         public static IntAmount<T> operator -(IntAmount<T> left, IntAmount<T> right)
         {
-            AssertEqualResource(left, right);
+            AssertEqualItem(left, right);
             return new(left.item, left.amount - right.amount);
         }
 
         public static bool operator >(IntAmount<T> left, IntAmount<T> right)
         {
-            AssertEqualResource(left, right);
+            AssertEqualItem(left, right);
             return left.amount > right.amount;
         }
 
         public static bool operator <(IntAmount<T> left, IntAmount<T> right)
         {
-            AssertEqualResource(left, right);
+            AssertEqualItem(left, right);
             return left.amount < right.amount;
         }
 
         public static bool operator >=(IntAmount<T> left, IntAmount<T> right)
         {
-            AssertEqualResource(left, right);
+            AssertEqualItem(left, right);
             return left.amount >= right.amount;
         }
 
         public static bool operator <=(IntAmount<T> left, IntAmount<T> right)
         {
-            AssertEqualResource(left, right);
+            AssertEqualItem(left, right);
             return left.amount <= right.amount;
         }
+
+        public static bool operator ==(IntAmount<T> left, IntAmount<T> right)
+        {
+            AssertEqualItem(left, right);
+            return left.amount == right.amount;
+        }
+
+        public static bool operator !=(IntAmount<T> left, IntAmount<T> right)
+        {
+            AssertEqualItem(left, right);
+            return left.amount != right.amount;
+        }
+
+        public override readonly bool Equals(object obj)
+        {
+            if (obj is IntAmount<T> other)
+            {
+                return this == other;
+            }
+            return false;
+        }
+
+        public override readonly int GetHashCode() => HashCode.Combine(item, amount);
 
     }
 
@@ -79,5 +104,21 @@ namespace GameDevKit
         public T item;
         public int amount;
         public float weight;
+    }
+
+    public static class IntAmountExtensions
+    {
+        public static bool HasEnough<T>(this IntAmount<T> left, IntAmount<T> right) => left >= right;
+
+        public static bool HasEnough<T>(this IEnumerable<IntAmount<T>> availableItems, IEnumerable<IntAmount<T>> requirements)
+        {
+            using var _ = DictionaryPool<T, int>.Get(out var amounts);
+            foreach (var item in availableItems)
+            {
+                amounts.TryAdd(item.item, 0);
+                amounts[item.item] += item.amount;
+            }
+            return requirements.All(ingredient => amounts.TryGetValue(ingredient.item, out var amount) && amount >= ingredient.amount);
+        }
     }
 }
